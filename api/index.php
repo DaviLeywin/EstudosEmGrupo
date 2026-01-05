4d4cc15a0fa7c8c957bda51e5321d25a26df8517
Davi Rodrigues de Almeida <davirralmeida@gmail.com>
2026-01-05 18:56:52 -0300

    Novo projeto de estudos.

---
+++ api/index.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+require_once __DIR__ . "\..\Framework.php";
+require_once "class.Rotas.php";
+require_once "class.Response.php";
+
+$rotas = new Rotas();
+
+// echo json_encode($rotas->Executar());
